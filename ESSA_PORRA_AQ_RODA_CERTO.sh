6e89eff39d98c8e72103081cced588b80a3b6415
Pedro Victor Abreu <pedro.fabreu97@gmail.com>
2024-11-02 03:53:19 -0300

    update

--- ESSA_PORRA_AQ_RODA_CERTO.sh
+++ ESSA_PORRA_AQ_RODA_CERTO.sh
@@ -3,22 +3,16 @@
 ENDPOINT="https://stsm.exostack.com.br/api/raspberry-scan-store"
 RASP_ID=1  # Substitua pelo ID do Raspberry Pi
 FILIAL_ID=1  # Substitua pelo ID da filial
-
 # HELLO WORD
-
 echo "Aguardando leitura do código de barras..."
-
 while true; do
     # Captura o código de barras do scanner
     read -r CODIGO  # Captura o input do scanner
-
     # Captura a data e hora atual
     DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
-
     # Envia o código de barras e outros dados para o endpoint
     curl -X POST -H "Content-Type: application/json" \
          -d "{\"codigo_barras\":\"$CODIGO\", \"data_time\":\"$DATETIME\", \"raspberry_id\":\"$RASP_ID\", \"filial_id\":\"$FILIAL_ID\"}" \
          $ENDPOINT
-
     # FIZ ISSO CHAPADO DE CERVEJA VIVA OCKTOBERFEST 
 done
