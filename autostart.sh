18af7f91ece46c84ff65099dd17e2495fdc48bd0
Pedro Victor Abreu <pedro.fabreu97@gmail.com>
2024-11-07 14:42:11 -0300

    update

---
+++ autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Ativa o ambiente virtual
+source /caminho/para/myenv/bin/activate
+# Instala as dependências
+pip install -r /caminho/para/requirements.txt
+# Executa o script Python
+python /caminho/para/script.py
