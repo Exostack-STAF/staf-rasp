63fd9a9f53133d242fa22407d1852d00c15bfaf0
Pedro Victor Abreu <pedro.fabreu97@gmail.com>
2024-11-07 15:00:08 -0300

    update

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# Ativa o ambiente virtual
-source /caminho/para/myenv/bin/activate
-# Instala as dependências
-pip install -r /caminho/para/requirements.txt
-# Executa o script Python
-python /caminho/para/script.py
+
+echo "Criando o ambiente virtual..."
+python -m venv /home/pi/staf-rasp/myenv
+
+echo "Ativando o ambiente virtual..."
+source /home/pi/staf-rasp/myenv/bin/activate
+
+echo "Instalando as dependências..."
+pip install -r /home/pi/staf-rasp/requirements.txt
+
+echo "Executando o script Python..."
+python /home/pi/staf-rasp/script.py
